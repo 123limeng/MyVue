93b0927722c0a03b601a2018f8c2a758afe2c04f
limeng <limengd@mail.taiji.com.cn>
2019-09-05 11:11:27 +0800

    路由

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <lay-out/>
   </div>
 </template>
 
 <script>
+import LayOut from './LayOut/index'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    LayOut
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
